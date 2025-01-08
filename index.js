558900cebcbe089b32b56cb68bf7ff079cd2b3de
Valencia Cheung <153874853+ValenciaCheung@users.noreply.github.com>
2025-01-08 14:14:48 -0800

    select the city clock

---
+++ index.js
@@ -0,0 +1,21 @@
+let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:m A");
+let newYorkTime = moment()
+  .tz("America/New_York")
+  .format("dddd, MMMM D, YYYY h:m A");
+let londonTime = moment()
+  .tz("Europe/London")
+  .format("dddd, MMMM D, YYYY h:m A");
+
+function displaySelectedCityTime(event) {
+  if (event.target.value === "Asia/Tokyo") {
+    alert(`It is ${tokyoTime} in Asia/Tokyo`);
+  }
+  if (event.target.value === "America/New_York") {
+    alert(`It is ${newYorkTime} in America/New_York`);
+  }
+  if (event.target.value === "Europe/London") {
+    alert(`It is ${londonTime} in Europe/London`);
+  }
+}
+let citiesSelector = document.querySelector("#clocks");
+citiesSelector.addEventListener("change", displaySelectedCityTime);
