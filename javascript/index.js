e1ff4b2df38c77e5ebe3df367c755ffa75490e3a
Valencia Cheung <153874853+ValenciaCheung@users.noreply.github.com>
2025-01-08 16:52:59 -0800

    add js and update clock interaction

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,41 @@
-let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:m A");
-let newYorkTime = moment()
-  .tz("America/New_York")
-  .format("dddd, MMMM D, YYYY h:m A");
-let londonTime = moment()
-  .tz("Europe/London")
-  .format("dddd, MMMM D, YYYY h:m A");
-let parisTime = moment().tz("Europe/Paris").format("dddd, MMMM D, YYYY h:m A");
+function updateClock() {
+  let tokyoElement = document.querySelector("#tokyo-city");
+  let tokyoTimeElement = document.querySelector("#tokyo-time");
+  let tokyoDateElement = document.querySelector("#tokyo-date");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("dddd, MMMM D, YYYY");
+  let timeWithoutPeriod = tokyoTime.format("hh:mm:ss");
+  let period = tokyoTime.format("A"); // A is for AM/PM
+  tokyoTimeElement.innerHTML = `${timeWithoutPeriod} <small>${period}</small>`;
 
+  let newYorkElement = document.querySelector("#newyork-city");
+  let newYorkTimeElement = document.querySelector("#newyork-time");
+  let newYorkDateElement = document.querySelector("#newyork-date");
+  let newYorkTime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = newYorkTime.format("dddd, MMMM D, YYYY");
+  let newYorkTimeWithoutPeriod = newYorkTime.format("hh:mm:ss");
+  let newYorkPeriod = newYorkTime.format("A"); // A is for AM/PM
+  newYorkTimeElement.innerHTML = `${newYorkTimeWithoutPeriod} <small>${newYorkPeriod}</small>`;
+
+  let londonElement = document.querySelector("#london-city");
+  let londonTimeElement = document.querySelector("#london-time");
+  let londonDateElement = document.querySelector("#london-date");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("dddd, MMMM D, YYYY");
+  let londonTimeWithoutPeriod = londonTime.format("hh:mm:ss");
+  let londonPeriod = londonTime.format("A"); // A is for AM/PM
+  londonTimeElement.innerHTML = `${londonTimeWithoutPeriod} <small>${londonPeriod}</small>`;
+
+  let parisElement = document.querySelector("#paris-city");
+  let parisTimeElement = document.querySelector("#paris-time");
+  let parisDateElement = document.querySelector("#paris-date");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("dddd, MMMM D, YYYY");
+  let parisTimeWithoutPeriod = parisTime.format("hh:mm:ss");
+  let parisPeriod = parisTime.format("A"); // A is for AM/PM
+  parisTimeElement.innerHTML = `${parisTimeWithoutPeriod} <small>${parisPeriod}</small>`;
+}
+setInterval(updateClock, 1000);
 function displaySelectedCityTime(event) {
   if (event.target.value === "Asia/Tokyo") {
     alert(`It is ${tokyoTime} in Asia/Tokyo`);
