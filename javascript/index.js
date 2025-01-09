2bb3c3a0592c3a77c0033214c691ed41029d9887
Valencia Cheung <153874853+ValenciaCheung@users.noreply.github.com>
2025-01-09 15:38:54 -0800

    add all cities link

--- javascript/index.js
+++ javascript/index.js
@@ -61,7 +61,8 @@ function updateCity(event) {
       <div class="time">${cityTime.format("hh:mm:ss")} <small>${cityTime.format(
     "A"
   )}</small></div>
-    </div>`;
+    </div>
+    <a href="/">All cities</a>`;
 }
 
 updateCityTime();
